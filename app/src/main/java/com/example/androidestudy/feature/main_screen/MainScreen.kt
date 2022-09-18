3630c2b52d4b435e97ea6e414a73ed474ba6b476
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-09-19 02:13:31 +0900

    [Authentication] modify MainScreen Button Width

--- app/src/main/java/com/example/androidestudy/feature/main_screen/MainScreen.kt
+++ app/src/main/java/com/example/androidestudy/feature/main_screen/MainScreen.kt
@@ -1,22 +1,16 @@
 package com.example.androidestudy.feature.presentation.main
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.androidestudy.R
 import com.example.androidestudy.feature.presentation.main.component.ExplainButton
 import com.example.androidestudy.feature.util.Screen
+import com.example.androidestudy.ui.theme.MAIN_SCREEN_BUTTON_WIDTH
 import com.example.androidestudy.ui.theme.MEDIUM_PADDING
-import com.example.androidestudy.ui.theme.SMALL_PADDING
 
 @Composable
 fun MainScreen(
@@ -30,6 +24,7 @@ fun MainScreen(
     ) {
         // Preferences DataStore
         ExplainButton(
+            modifier = Modifier.width(MAIN_SCREEN_BUTTON_WIDTH),
             explain = stringResource(id = R.string.preferences_datastore),
             onClick = {
                 navController.navigate(Screen.SplashScreen.route)
@@ -40,6 +35,7 @@ fun MainScreen(
         
         // Firebase Authentication
         ExplainButton(
+            modifier = Modifier.width(MAIN_SCREEN_BUTTON_WIDTH),
             explain = stringResource(id = R.string.authentication),
             onClick = {
                 navController.navigate(Screen.SignInScreen.route)
