a0c3c98b676d9d54615e1bde35ef06394fafdfe6
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-09-25 22:48:56 +0900

    [Authentication] Implement SignIn Screen Component

---
+++ app/src/main/java/com/example/androidestudy/feature/auth/presentation/component/BottomAreaComponent.kt
@@ -0,0 +1,74 @@
+package com.example.androidestudy.feature.auth.presentation.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import com.example.androidestudy.R
+import com.example.androidestudy.ui.theme.MEDIUM_PADDING
+
+@Composable
+fun BottomAreaComponent(
+    buttonTitle: String,
+    haveAccount: String,
+    operateTitle: String,
+    onClick: () -> Unit,
+    onNavigate: () -> Unit
+) {
+    Column {
+        Button(
+            modifier = Modifier
+                .padding(MEDIUM_PADDING)
+                .fillMaxWidth(),
+            onClick = onClick
+        ) {
+            Text(
+                text = buttonTitle,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            text = stringResource(id = R.string.or)
+        )
+
+        Row(
+            modifier = Modifier
+                .padding(top = MEDIUM_PADDING)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = haveAccount
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                modifier = Modifier.clickable {
+                    // = でつなぐ以外は、ラムダをラムダないでメソッドのように呼び出す必要がある
+                    onNavigate()
+                },
+                text = operateTitle,
+                style = TextStyle(
+                    textDecoration = TextDecoration.Underline
+                ),
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/androidestudy/feature/auth/presentation/sign_in/component/preview/SignInScreenPreView.kt
+++ app/src/main/java/com/example/androidestudy/feature/auth/presentation/sign_in/component/preview/SignInScreenPreView.kt
@@ -1,30 +1,23 @@
 package com.example.androidestudy.feature.auth.presentation.sign_in.component.preview
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androidestudy.R
+import com.example.androidestudy.feature.auth.presentation.component.BottomAreaComponent
 import com.example.androidestudy.feature.auth.presentation.component.StandardTextField
-import com.example.androidestudy.ui.theme.MEDIUM_PADDING
 
 @Preview
 @Composable
@@ -65,39 +58,12 @@ fun SignInScreenPreView() {
             onValueChange = {},
             onClick = { /*TODO*/ })
 
-        Button(
-            modifier = Modifier
-                .padding(MEDIUM_PADDING)
-                .fillMaxWidth(),
-            onClick = { /*TODO*/ }
-        ) {
-            Text(text = "Sign In")
-        }
-
-        Text(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-            text = "OR"
+        BottomAreaComponent(
+            buttonTitle = stringResource(id = R.string.sign_in),
+            haveAccount = stringResource(id = R.string.have_account),
+            operateTitle = stringResource(id = R.string.login),
+            onClick = {},
+            onNavigate = {}
         )
-
-        Row(
-            modifier = Modifier
-                .padding(top = MEDIUM_PADDING)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Already have a Account?"
-            )
-            Spacer(modifier = Modifier.width(5.dp))
-            Text(
-                modifier = Modifier.clickable {  },
-                text = "LOGIN",
-                style = TextStyle(
-                    textDecoration = TextDecoration.Underline
-                ),
-                fontWeight = FontWeight.Bold
-            )
-        }
     }
 }
