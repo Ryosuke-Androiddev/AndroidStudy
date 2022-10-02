a82ce2bef87f309ca8c641fb210455f1613806dd
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-10-02 18:54:19 +0900

    [Retrofit] Implement UseCase Logic

---
+++ app/src/main/java/com/example/androidestudy/feature/retrofit/domain/model/UserOperationResult.kt
@@ -0,0 +1,6 @@
+package com.example.androidestudy.feature.retrofit.domain.model
+
+data class UserOperationResult(
+    val statusCode: String? = null,
+    val textInputValidationResult: Boolean
+)
