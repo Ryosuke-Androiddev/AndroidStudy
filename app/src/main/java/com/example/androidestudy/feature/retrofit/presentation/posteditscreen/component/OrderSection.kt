15bb72bf17c768baf277742882ad4b9a31f1171f
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-10-18 23:15:23 +0900

    [Retrofit2] delete no need line

--- app/src/main/java/com/example/androidestudy/feature/retrofit/presentation/posteditscreen/component/OrderSection.kt
+++ app/src/main/java/com/example/androidestudy/feature/retrofit/presentation/posteditscreen/component/OrderSection.kt
@@ -1,16 +1,11 @@
 package com.example.androidestudy.feature.retrofit.presentation.posteditscreen.component
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.androidestudy.feature.retrofit.domain.model.order.OrderType
 import com.example.androidestudy.feature.retrofit.domain.model.order.PostOrder
 
