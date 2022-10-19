a0c7878909261d37983c00b587ff73e7d29adbbb
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-10-19 22:44:22 +0900

    [Retrofit2] Modify PostUserPostUseCase

---
+++ app/src/main/java/com/example/androidestudy/feature/retrofit/domain/model/util/ScreenState.kt
@@ -0,0 +1,12 @@
+package com.example.androidestudy.feature.retrofit.domain.model.util
+
+import com.example.androidestudy.feature.retrofit.domain.model.TextInputValidationResult
+
+sealed class ScreenState {
+    object Success: ScreenState()
+    object Failure: ScreenState()
+    data class TextInputError(
+        val titleInputValidationResult: TextInputValidationResult,
+        val bodyInputValidationResult: TextInputValidationResult
+    ): ScreenState()
+}
