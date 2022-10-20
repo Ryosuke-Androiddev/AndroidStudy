f0b9eef9e69e854daccc963113e827b464ba21a1
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-10-20 16:01:48 +0900

    [Retrofit2] Modify UpdateUserPostUseCase Implementation

--- app/src/main/java/com/example/androidestudy/feature/retrofit/domain/usecase/UpdateUserPostUseCase.kt
+++ app/src/main/java/com/example/androidestudy/feature/retrofit/domain/usecase/UpdateUserPostUseCase.kt
@@ -1,15 +1,15 @@
 package com.example.androidestudy.feature.retrofit.domain.usecase
 
-import com.example.androidestudy.feature.retrofit.domain.model.UserOperationResult
 import com.example.androidestudy.feature.retrofit.domain.model.UserPostItem
+import com.example.androidestudy.feature.retrofit.domain.model.result.UpdateUserPostState
+import com.example.androidestudy.feature.retrofit.domain.model.util.ScreenState
 import com.example.androidestudy.feature.retrofit.domain.repository.UserPostRepository
-import kotlin.Exception
 
 class UpdateUserPostUseCase(
     private val repository: UserPostRepository,
     private val textInputValidationUseCase: TextInputValidationUseCase
 ) {
-    suspend operator fun invoke(userPostItem: UserPostItem): Result<UserOperationResult> {
+    suspend operator fun invoke(userPostItem: UserPostItem): ScreenState {
 
         // userIdってどうやって振り分けられる??
         val title = textInputValidationUseCase.validate(userPostItem.title)
@@ -23,44 +23,16 @@ class UpdateUserPostUseCase(
         }
 
         if (hasError) {
-            // 文字列の長さを確認しているので、ステータスコードは省略
-            // ViewModelは、以下のValidationResultを使ってstateを更新する
-            val userOperationResult = UserOperationResult(
-                textInputValidationResult = false
-            )
-            // 厳密には成功していない
-            // 例外処理と文字列が足りる足りないを分けたいので以下のような処理を実装
-            // PostUserPostUseCase.onSuccess {
-            //     if (!it.textInputValidationResult)
-            // }
-            // 上記のような処理が必要になる
-            return Result.success(userOperationResult)
+            return ScreenState.TextInputError
         }
 
-        return try {
-
-            val statusCode = repository.updatePost(userPostItem = userPostItem)
-
-            // ViewModelは、以下のValidationResultを使ってstateを更新する
-            // Defaultでは、何らかの通信エラーが発生したときを想定
-            // サーバー側のエラーとして処理する
-            val userOperationResult = UserOperationResult(
-                statusCode = SUCCESS_DEFAULT_VALUE,
-                textInputValidationResult = false
-            )
-            // ViewModelは、以下のValidationResultを使ってstateを更新する
-            Result.success(userOperationResult)
-        } catch (e: Exception) {
-            val userFailureOperationResult = UserOperationResult(
-                statusCode = FAILURE_DEFAULT_VALUE,
-                textInputValidationResult = false
-            )
-            Result.success(userFailureOperationResult)
+        return when (repository.updatePost(userPostItem = userPostItem)) {
+            is UpdateUserPostState.UpdateUserPost -> {
+                ScreenState.Success
+            }
+            is UpdateUserPostState.Failure -> {
+                ScreenState.Failure
+            }
         }
-    }
-
-    companion object {
-        private const val SUCCESS_DEFAULT_VALUE = "200"
-        private const val FAILURE_DEFAULT_VALUE = "500"
     }
 }
