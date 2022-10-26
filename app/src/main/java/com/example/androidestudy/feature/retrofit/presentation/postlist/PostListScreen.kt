1115bcac86d0d1bc6171a48c50b418c7b8a0d99e
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-10-26 15:51:18 +0900

    [Retrofit2] Implement Navigation Screen Component

---
+++ app/src/main/java/com/example/androidestudy/feature/retrofit/presentation/postlist/PostListScreen.kt
@@ -0,0 +1,17 @@
+package com.example.androidestudy.feature.retrofit.presentation.postlist
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun PostListScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "PostListScreen")
+    }
+}
---
+++ app/src/main/java/com/example/androidestudy/feature/retrofit/presentation/postscreen/PostScreen.kt
@@ -0,0 +1,17 @@
+package com.example.androidestudy.feature.retrofit.presentation.postscreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun PostScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "PostListScreen")
+    }
+}
