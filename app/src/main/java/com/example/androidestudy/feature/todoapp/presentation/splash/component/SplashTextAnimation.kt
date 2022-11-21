aead8b4778660f42f8e60cc26572006dae0cebc1
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-11-22 00:03:02 +0900

    [TodoApp] Implement SplashScreen Text Animation

--- app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/splash/component/SplashTextAnimation.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/splash/component/SplashTextAnimation.kt
@@ -1,22 +1,20 @@
 package com.example.androidestudy.feature.todoapp.presentation.splash.component
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.repeatable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
@@ -32,17 +30,29 @@ fun SplashTextAnimation(
         remember { Animatable(initialValue = 0f) },
         remember { Animatable(initialValue = 0f) },
         remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) },
     )
 
     texts.forEachIndexed { index, animatable ->
         LaunchedEffect(key1 = animatable) {
             // ここで遅延をかける
-            delay(index + 100L)
+            delay(index * 100L)
+            // targetValueで、成長させなければ最後に上に移動するアニメーションがなくなる
             animatable.animateTo(
-                targetValue = 1f,
-                animationSpec = infiniteRepeatable(
+                targetValue = 0f,
+                animationSpec = repeatable(
+                    iterations = 1,
                     animation = keyframes {
-
+                        durationMillis = 1600
+                        // start
+                        0.0f at 0 with LinearOutSlowInEasing
+                        // 一番上までいく
+                        1.0f at 400 with LinearOutSlowInEasing
+                        // startポジションに戻る
+                        0.0f at 800 with LinearOutSlowInEasing
+                        // startポジションで待機
+                        0.0f at 1600 with LinearOutSlowInEasing
                     },
                     repeatMode = RepeatMode.Restart
                 )
@@ -61,16 +71,13 @@ fun SplashTextAnimation(
         horizontalArrangement = Arrangement.spacedBy(spaceBetween)
     ) {
         textValues.forEach { value ->
-            Box(
+            Text(
                 modifier = Modifier
-                    .size(20.dp)
                     .graphicsLayer {
                         translationY = -value * distance
-                    }
-                    .background(
-                        color = MaterialTheme.colors.primary,
-                        shape = CircleShape
-                    )
+                    },
+                text = "A",
+                fontWeight = FontWeight.Bold
             )
         }
     }
