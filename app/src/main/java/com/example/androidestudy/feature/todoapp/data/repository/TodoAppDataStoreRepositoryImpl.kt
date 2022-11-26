5090789f0e98d4b2139589fa9df32977dbdb059f
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-11-26 20:49:42 +0900

    [TodoApp] Setup DataStore

---
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/data/repository/TodoAppDataStoreRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.example.androidestudy.feature.todoapp.data.repository
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.androidestudy.feature.todoapp.data.repository.TodoAppDataStoreRepositoryImpl.Companion.TODO_PREFERENCES_DATA_STORE_NAME
+import com.example.androidestudy.feature.todoapp.domain.repository.TodoAppDataStoreRepository
+import kotlinx.coroutines.flow.Flow
+
+// トップレベルかつContext経由のDelegateで、Singleton ProcessなDataStoreを提供できる
+// DI経由で渡すだけで、SingletonなDataStoreを参照できる
+val Context.dataStore : DataStore<Preferences> by preferencesDataStore(name = TODO_PREFERENCES_DATA_STORE_NAME)
+
+class TodoAppDataStoreRepositoryImpl(
+    context: Context
+): TodoAppDataStoreRepository {
+
+    // Delegateして、DataStoreを保持するとSingletonなDataStoreとなって、Hiltと相性がいい
+    // レシーバーとしてContext経由でDataStoreを取得する
+    // キーを使ってプリミティブ型で保持、編集できる
+    // キーはこのクラス内でカプセル化して利用する
+    private val dataStore = context.dataStore
+
+    override fun getOnBoardingState(): Flow<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun changeOnBoardingState() {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        const val TODO_PREFERENCES_DATA_STORE_NAME = "todo_data_store"
+    }
+}
---
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/domain/repository/TodoAppDataStoreRepository.kt
@@ -0,0 +1,10 @@
+package com.example.androidestudy.feature.todoapp.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface TodoAppDataStoreRepository {
+    // 読み込みは、Flowで値を取ってくるから非同期関数
+    fun getOnBoardingState(): Flow<Boolean>
+    // 書き込みは、suspend関数にして非同期で処理
+    suspend fun changeOnBoardingState()
+}
