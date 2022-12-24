d686c903b9f499abe9a33e706c2e2ecc752d314d
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-12-24 20:42:17 +0900

    [TodoApp] setup UseCase

--- app/src/main/java/com/example/androidestudy/feature/todoapp/domain/usecase/todo/GetAllTodo.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/domain/usecase/todo/GetAllTodo.kt
@@ -1,4 +1,16 @@
 package com.example.androidestudy.feature.todoapp.domain.usecase.todo
 
-class GetAllTodo {
+import com.example.androidestudy.feature.todoapp.domain.model.todo.TodoItem
+import com.example.androidestudy.feature.todoapp.domain.repository.TodoLocalDBRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAllTodo @Inject constructor(
+    private val repository: TodoLocalDBRepository
+) {
+
+    // sort 一覧 title, content, priority, createdAt
+    operator fun invoke(): Flow<List<TodoItem>> {
+        return repository.getAllTodo()
+    }
 }
