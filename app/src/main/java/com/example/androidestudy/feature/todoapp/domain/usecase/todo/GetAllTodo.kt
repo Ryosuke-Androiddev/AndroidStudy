f124b0790a8d54809e59f53b066596a30500fcfe
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-12-28 23:58:43 +0900

    [TodoApp] Implement GetAll Todo List UseCase

--- app/src/main/java/com/example/androidestudy/feature/todoapp/domain/usecase/todo/GetAllTodo.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/domain/usecase/todo/GetAllTodo.kt
@@ -1,15 +1,11 @@
 package com.example.androidestudy.feature.todoapp.domain.usecase.todo
 
 import com.example.androidestudy.feature.todoapp.domain.mapper.toTodoItemState
-import com.example.androidestudy.feature.todoapp.domain.model.todo.TodoItem
 import com.example.androidestudy.feature.todoapp.domain.model.todo.order.OrderType
 import com.example.androidestudy.feature.todoapp.domain.model.todo.order.TodoPostOrder
 import com.example.androidestudy.feature.todoapp.domain.repository.TodoLocalDBRepository
-import com.example.androidestudy.feature.todoapp.presentation.home.component.Priority
-import com.example.androidestudy.feature.todoapp.presentation.home.component.TodoPriority
 import com.example.androidestudy.feature.todoapp.presentation.home.component.todo.TodoItemState
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.forEach
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
@@ -21,9 +17,40 @@ class GetAllTodo @Inject constructor(
     operator fun invoke(
         todoPostOrder: TodoPostOrder = TodoPostOrder.Date(orderType = OrderType.Descending)
     ): Flow<List<TodoItemState>> {
-        return repository.getAllTodo().map { todoList ->
+        val todoList = repository.getAllTodo().map { todoList ->
             todoList.map { todoItem ->
                 todoItem.toTodoItemState()
+            }
+        }
+
+        return todoList.map { todoStateList ->
+            when (todoPostOrder.orderType) {
+                is OrderType.Ascending -> {
+                    when (todoPostOrder) {
+                        is TodoPostOrder.Date -> {
+                            todoStateList.sortedBy { it.createdAt }
+                        }
+                        is TodoPostOrder.Priority -> {
+                            todoStateList.sortedBy { it.priority.order }
+                        }
+                        is TodoPostOrder.Title -> {
+                            todoStateList.sortedBy { it.title }
+                        }
+                    }
+                }
+                is OrderType.Descending -> {
+                    when (todoPostOrder) {
+                        is TodoPostOrder.Date -> {
+                            todoStateList.sortedByDescending { it.createdAt }
+                        }
+                        is TodoPostOrder.Priority -> {
+                            todoStateList.sortedByDescending { it.priority.order }
+                        }
+                        is TodoPostOrder.Title -> {
+                            todoStateList.sortedByDescending { it.title }
+                        }
+                    }
+                }
             }
         }
     }
