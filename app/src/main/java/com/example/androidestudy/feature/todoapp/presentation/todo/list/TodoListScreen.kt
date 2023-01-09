08ef18722c236fcdcd5693c2115dda59f7357f3a
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-01-09 21:33:36 +0900

    [TodoApp] Implement TodoList Screen

--- app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/list/TodoListScreen.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/list/TodoListScreen.kt
@@ -1,18 +1,28 @@
 package com.example.androidestudy.feature.todoapp.presentation.todo.list
 
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.androidestudy.feature.todoapp.presentation.home.viewmodel.HomeViewModel
+import com.example.androidestudy.feature.todoapp.presentation.todo.component.TodoGridItem
+import com.example.androidestudy.feature.todoapp.presentation.todo.list.component.SearchBar
+import com.example.androidestudy.feature.todoapp.presentation.todo.list.component.TodoListEvent
 import com.example.androidestudy.feature.todoapp.presentation.todo.list.viewmodel.TodoListViewModel
+import com.example.androidestudy.feature.todoapp.presentation.util.fakeTodoList
 
 // 最初のViewModelがここに入ってるからリソースが共有できていない
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TodoListScreen(
     priority: Int,
@@ -21,12 +31,46 @@ fun TodoListScreen(
     val state = viewModel.state
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxSize()
+            .padding(top = 12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        state.todoListByPriority.forEach {
-            Text(text = it.title)
+        SearchBar(
+            text = state.query,
+            onValueChange = {
+                viewModel.onEvent(TodoListEvent.EnterSearchQuery(it))
+            },
+            onClear = {
+                viewModel.onEvent(TodoListEvent.ClearSearchQuery)
+            }
+        )
+        
+        Spacer(modifier = Modifier.height(8.dp))
+        
+        if (priority != -1) {
+            LazyVerticalGrid(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                cells = GridCells.Fixed(2),
+                content = {
+                    // state.todoListByPriority へ変更
+                    items(fakeTodoList) {
+                        TodoGridItem(title = it.title, content = it.content, priority = it.priority.name)
+                    }
+                }
+            )
+        } else {
+            LazyVerticalGrid(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                cells = GridCells.Fixed(2),
+                content = {
+                    // state.todoItemListへ変更
+                    items(fakeTodoList) {
+                        TodoGridItem(title = it.title, content = it.content, priority = it.priority.name)
+                    }
+                }
+            )
         }
     }
 }
