2ab004322c19fd47cb296235b8b12742e6d4541c
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-01-12 13:37:55 +0900

    [TodoApp] setup Schedule Header

--- app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/DailyScreen.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/DailyScreen.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.androidestudy.feature.todoapp.presentation.todo.daily.component.ScheduleHeader
 
 @Composable
-fun DailyScreen() {
+fun DailyScreen(
+    navController: NavController
+) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "A")
+        ScheduleHeader(navController = navController, month = "2月")
     }
 }
