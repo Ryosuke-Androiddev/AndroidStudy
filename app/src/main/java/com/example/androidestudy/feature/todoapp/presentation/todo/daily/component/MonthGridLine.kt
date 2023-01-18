7e322a572a7f31cd3309508877f5b3d550c996f6
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-01-18 12:07:58 +0900

    [TodoApp] setup Month day Layout

--- app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/component/MonthGridLine.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/component/MonthGridLine.kt
@@ -34,6 +34,39 @@ fun MonthGridLine() {
         "土",
         "日",
     )
+    val calendar = listOf(
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
+        25,
+        26,
+        27,
+        28,
+        29,
+        30,
+        31
+    )
     // 曜日を均等に並べる処理を描く
     Layout(
         modifier = Modifier
@@ -135,6 +168,15 @@ fun MonthGridLine() {
                     fontWeight = FontWeight.Bold
                 )
             }
+            calendar.forEachIndexed { index, s ->
+                val layoutId = index + 10
+                Text(
+                    modifier = Modifier.layoutId("$layoutId"),
+                    text = "1",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         },
         measurePolicy = { measurables, constraints ->
 
@@ -153,6 +195,69 @@ fun MonthGridLine() {
                 ?: error("title not found")
             val sundayMeasurable = measurables.find { it.layoutId == "6" }
                 ?: error("title not found")
+
+            val dayOneMeasurable = measurables.find { it.layoutId == "10" }
+                ?: error("not found")
+            val dayTwoMeasurable = measurables.find { it.layoutId == "11" }
+                ?: error("not found")
+            val dayThreeMeasurable = measurables.find { it.layoutId == "12" }
+                ?: error("not found")
+            val dayFourMeasurable = measurables.find { it.layoutId == "13" }
+                ?: error("not found")
+            val dayFiveMeasurable = measurables.find { it.layoutId == "14" }
+                ?: error("not found")
+            val daySixMeasurable = measurables.find { it.layoutId == "15" }
+                ?: error("not found")
+            val daySevenMeasurable = measurables.find { it.layoutId == "16" }
+                ?: error("not found")
+            val dayEightMeasurable = measurables.find { it.layoutId == "17" }
+                ?: error("not found")
+            val dayNineMeasurable = measurables.find { it.layoutId == "18" }
+                ?: error("not found")
+            val dayTenMeasurable = measurables.find { it.layoutId == "19" }
+                ?: error("not found")
+            val dayElevenMeasurable = measurables.find { it.layoutId == "20" }
+                ?: error("not found")
+            val dayTwelveMeasurable = measurables.find { it.layoutId == "21" }
+                ?: error("not found")
+            val dayThirteenMeasurable = measurables.find { it.layoutId == "22" }
+                ?: error("not found")
+            val dayFourteenMeasurable = measurables.find { it.layoutId == "23" }
+                ?: error("not found")
+            val dayFifteenMeasurable = measurables.find { it.layoutId == "24" }
+                ?: error("not found")
+            val daySixteenMeasurable = measurables.find { it.layoutId == "25" }
+                ?: error("not found")
+            val daySeventeenMeasurable = measurables.find { it.layoutId == "26" }
+                ?: error("not found")
+            val dayEighteenMeasurable = measurables.find { it.layoutId == "27" }
+                ?: error("not found")
+            val dayNineteenMeasurable = measurables.find { it.layoutId == "28" }
+                ?: error("not found")
+            val dayTwentyMeasurable = measurables.find { it.layoutId == "29" }
+                ?: error("not found")
+            val dayTwentyOneMeasurable = measurables.find { it.layoutId == "30" }
+                ?: error("not found")
+            val dayTwentyTwoMeasurable = measurables.find { it.layoutId == "31" }
+                ?: error("not found")
+            val dayTwentyThreeMeasurable = measurables.find { it.layoutId == "32" }
+                ?: error("not found")
+            val dayTwentyFourMeasurable = measurables.find { it.layoutId == "33" }
+                ?: error("not found")
+            val dayTwentyFiveMeasurable = measurables.find { it.layoutId == "34" }
+                ?: error("not found")
+            val dayTwentySixMeasurable = measurables.find { it.layoutId == "35" }
+                ?: error("not found")
+            val dayTwentySevenMeasurable = measurables.find { it.layoutId == "36" }
+                ?: error("not found")
+            val dayTwentyEightMeasurable = measurables.find { it.layoutId == "37" }
+                ?: error("not found")
+            val dayTwentyNineMeasurable = measurables.find { it.layoutId == "38" }
+                ?: error("not found")
+            val dayThirtyMeasurable = measurables.find { it.layoutId == "39" }
+                ?: error("not found")
+            val dayThirtyOneMeasurable = measurables.find { it.layoutId == "40" }
+                ?: error("not found")
 
             val mondayConstraints = constraints.copy(
                 minHeight = 0,
@@ -183,6 +288,131 @@ fun MonthGridLine() {
                 minWidth = 0
             )
 
+            val dayOneConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwoConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayThreeConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayFourConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayFiveConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val daySixConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val daySevenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayEightConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayNineConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayElevenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwelveConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayThirteenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayFourteenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayFifteenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val daySixteenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val daySeventeenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayEighteenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayNineteenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyOneConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyTwoConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyThreeConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyFourConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyFiveConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentySixConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentySevenConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyEightConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayTwentyNineConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayThirtyConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+            val dayThirtyOneConstraints = constraints.copy(
+                minHeight = 0,
+                minWidth = 0
+            )
+
             val mondayPlaceable = mondayMeasurable.measure(mondayConstraints)
             val tuesdayPlaceable = tuesdayMeasurable.measure(tuesdayConstraints)
             val wednesdayPlaceable = wednesMeasurable.measure(wednesdayConstraints)
@@ -190,6 +420,38 @@ fun MonthGridLine() {
             val fridayPlaceable = fridayMeasurable.measure(fridayConstraints)
             val saturdayPlaceable = saturdayMeasurable.measure(saturdayConstraints)
             val sundayPlaceable = sundayMeasurable.measure(sundayConstraints)
+
+            val dayOnePlaceable = dayOneMeasurable.measure(dayOneConstraints)
+            val dayTwoPlaceable = dayTwoMeasurable.measure(dayTwoConstraints)
+            val dayThreePlaceable = dayThreeMeasurable.measure(dayThreeConstraints)
+            val dayFourPlaceable = dayFourMeasurable.measure(dayFourConstraints)
+            val dayFivePlaceable = dayFiveMeasurable.measure(dayFiveConstraints)
+            val daySixPlaceable = daySixMeasurable.measure(daySixConstraints)
+            val daySevenPlaceable = daySevenMeasurable.measure(daySevenConstraints)
+            val dayEightPlaceable = dayEightMeasurable.measure(dayEightConstraints)
+            val dayNinePlaceable = dayNineMeasurable.measure(dayNineConstraints)
+            val dayTenPlaceable = dayTenMeasurable.measure(dayTenConstraints)
+            val dayElevenPlaceable = dayElevenMeasurable.measure(dayElevenConstraints)
+            val dayTwelvePlaceable = dayTwelveMeasurable.measure(dayTwelveConstraints)
+            val dayThirteenPlaceable = dayThirteenMeasurable.measure(dayThirteenConstraints)
+            val dayFourteenPlaceable = dayFourteenMeasurable.measure(dayFourteenConstraints)
+            val dayFifteenPlaceable = dayFifteenMeasurable.measure(dayFifteenConstraints)
+            val daySixteenPlaceable = daySixteenMeasurable.measure(daySixteenConstraints)
+            val daySeventeenPlaceable = daySeventeenMeasurable.measure(daySeventeenConstraints)
+            val dayEighteenPlaceable = dayEighteenMeasurable.measure(dayEighteenConstraints)
+            val dayNineteenPlaceable = dayNineteenMeasurable.measure(dayNineteenConstraints)
+            val dayTwentyPlaceable = dayTwentyMeasurable.measure(dayTwentyConstraints)
+            val dayTwentyOnePlaceable = dayTwentyOneMeasurable.measure(dayTwentyOneConstraints)
+            val dayTwentyTwoPlaceable = dayTwentyTwoMeasurable.measure(dayTwentyTwoConstraints)
+            val dayTwentyThreePlaceable = dayTwentyThreeMeasurable.measure(dayTwentyThreeConstraints)
+            val dayTwentyFourPlaceable = dayTwentyFourMeasurable.measure(dayTwentyFourConstraints)
+            val dayTwentyFivePlaceable = dayTwentyFiveMeasurable.measure(dayTwentyFiveConstraints)
+            val dayTwentySixPlaceable = dayTwentySixMeasurable.measure(dayTwentySixConstraints)
+            val dayTwentySevenPlaceable = dayTwentySevenMeasurable.measure(dayTwentySevenConstraints)
+            val dayTwentyEightPlaceable = dayTwentyEightMeasurable.measure(dayTwentyEightConstraints)
+            val dayTwentyNinePlaceable = dayTwentyNineMeasurable.measure(dayTwentyNineConstraints)
+            val dayThirtyPlaceable = dayThirtyMeasurable.measure(dayThirtyConstraints)
+            val dayThirtyOnePlaceable = dayThirtyOneMeasurable.measure(dayThirtyOneConstraints)
 
             val width = constraints.maxWidth
             val height = maxOf(
@@ -202,23 +464,10 @@ fun MonthGridLine() {
                 sundayPlaceable.height
             )
 
-            layout(width, height) {
-                val space = IntSize(width, height)
-                val mondaySize = IntSize(mondayPlaceable.width, mondayPlaceable.height)
-                val tuesdaySize = IntSize(tuesdayPlaceable.width, tuesdayPlaceable.height)
-                val wednesdaySize = IntSize(wednesdayPlaceable.width, wednesdayPlaceable.height)
-                val thursdaySize = IntSize(thursdayPlaceable.width, thursdayPlaceable.height)
-                val fridaySize = IntSize(fridayPlaceable.width, fridayPlaceable.height)
-                val saturdaySize = IntSize(saturdayPlaceable.width, saturdayPlaceable.height)
-                val sundaySize = IntSize(sundayPlaceable.width, sundayPlaceable.height)
+            // val dayWidth = constraints.maxWidth
+            // val dayHeight = dayOnePlaceable.height
 
-                // val mondayOffset = Alignment.Center.align(mondaySize, space, layoutDirection)
-                // val tuesdayOffset = Alignment.Center.align(tuesdaySize, space, layoutDirection)
-                // val wednesdayOffset = Alignment.Center.align(wednesdaySize, space, layoutDirection)
-                // val thursdayOffset = Alignment.Center.align(thursdaySize, space, layoutDirection)
-                // val fridayOffset = Alignment.Center.align(fridaySize, space, layoutDirection)
-                // val saturdayOffset = Alignment.Center.align(saturdaySize, space, layoutDirection)
-                // val sundayOffset = Alignment.Center.align(sundaySize, space, layoutDirection)
+            layout(width, height) {
 
                 val sundayOffset = IntOffset(30, 0)
                 val mondayOffsets = IntOffset(189, 0)
@@ -236,13 +485,69 @@ fun MonthGridLine() {
                 fridayPlaceable.place(fridayOffsets)
                 saturdayPlaceable.place(saturdayOffsets)
 
-                // mondayPlaceable.place(mondayOffset)
-                // tuesdayPlaceable.place(tuesdayOffset)
-                // wednesdayPlaceable.place(wednesdayOffset)
-                // thursdayPlaceable.place(thursdayOffset)
-                // fridayPlaceable.place(fridayOffset)
-                // saturdayPlaceable.place(saturdayOffset)
-                // sundayPlaceable.place(sundayOffset)
+                val dayOneOffset = IntOffset(35, 100)
+                val dayTwoOffset = IntOffset(197, 100)
+                val dayThreeOffset = IntOffset(351, 100)
+                val dayFourOffset = IntOffset(508, 100)
+                val dayFiveOffset = IntOffset(662, 100)
+                val daySixOffset = IntOffset(812, 100)
+                val daySevenOffset = IntOffset(965, 100)
+                val dayEightOffset = IntOffset(35, 430)
+                val dayNineOffset = IntOffset(197, 430)
+                val dayTenOffset = IntOffset(351, 430)
+                val dayElevenOffset = IntOffset(508, 430)
+                val dayTwelveOffset = IntOffset(662, 430)
+                val dayThirteenOffset = IntOffset(812, 430)
+                val dayFourteenOffset = IntOffset(965, 430)
+                val dayFifteenOffset = IntOffset(35, 760)
+                val daySixteenOffset = IntOffset(197, 760)
+                val daySeventeenOffset = IntOffset(351, 760)
+                val dayEighteenOffset = IntOffset(508, 760)
+                val dayNineteenOffset = IntOffset(662, 760)
+                val dayTwentyOffset = IntOffset(812, 760)
+                val dayTwentyOneOffset = IntOffset(965, 760)
+                val dayTwentyTwoOffset = IntOffset(35, 1090)
+                val dayTwentyThreeOffset = IntOffset(197, 1090)
+                val dayTwentyFourOffset = IntOffset(351, 1090)
+                val dayTwentyFiveOffset = IntOffset(508, 1090)
+                val dayTwentySixOffset = IntOffset(662, 1090)
+                val dayTwentySevenOffset = IntOffset(812, 1090)
+                val dayTwentyEightOffset = IntOffset(965, 1090)
+                val dayTwentyNineOffset = IntOffset(35, 1410)
+                val dayThirtyOffset = IntOffset(197, 1410)
+                val dayThirtyOneOffset = IntOffset(351, 1410)
+
+                dayOnePlaceable.place(dayOneOffset)
+                dayTwoPlaceable.place(dayTwoOffset)
+                dayThreePlaceable.place(dayThreeOffset)
+                dayFourPlaceable.place(dayFourOffset)
+                dayFivePlaceable.place(dayFiveOffset)
+                daySixPlaceable.place(daySixOffset)
+                daySevenPlaceable.place(daySevenOffset)
+                dayEightPlaceable.place(dayEightOffset)
+                dayNinePlaceable.place(dayNineOffset)
+                dayTenPlaceable.place(dayTenOffset)
+                dayElevenPlaceable.place(dayElevenOffset)
+                dayTwelvePlaceable.place(dayTwelveOffset)
+                dayThirteenPlaceable.place(dayThirteenOffset)
+                dayFourteenPlaceable.place(dayFourteenOffset)
+                dayFifteenPlaceable.place(dayFifteenOffset)
+                daySixteenPlaceable.place(daySixteenOffset)
+                daySeventeenPlaceable.place(daySeventeenOffset)
+                dayEighteenPlaceable.place(dayEighteenOffset)
+                dayNineteenPlaceable.place(dayNineteenOffset)
+                dayTwentyPlaceable.place(dayTwentyOffset)
+                dayTwentyOnePlaceable.place(dayTwentyOneOffset)
+                dayTwentyTwoPlaceable.place(dayTwentyTwoOffset)
+                dayTwentyThreePlaceable.place(dayTwentyThreeOffset)
+                dayTwentyFourPlaceable.place(dayTwentyFourOffset)
+                dayTwentyFivePlaceable.place(dayTwentyFiveOffset)
+                dayTwentySixPlaceable.place(dayTwentySixOffset)
+                dayTwentySevenPlaceable.place(dayTwentySevenOffset)
+                dayTwentyEightPlaceable.place(dayTwentyEightOffset)
+                dayTwentyNinePlaceable.place(dayTwentyNineOffset)
+                dayThirtyPlaceable.place(dayThirtyOffset)
+                dayThirtyOnePlaceable.place(dayThirtyOneOffset)
             }
         }
     )
