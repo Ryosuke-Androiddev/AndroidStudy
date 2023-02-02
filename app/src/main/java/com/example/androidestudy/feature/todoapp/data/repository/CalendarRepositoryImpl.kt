00848fe31a999622d64faf18e83209f6a3ecb7e7
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-02-03 01:04:57 +0900

    [TodoApp] Implement Domain Logic

--- app/src/main/java/com/example/androidestudy/feature/todoapp/data/repository/CalendarRepositoryImpl.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/data/repository/CalendarRepositoryImpl.kt
@@ -16,23 +16,24 @@ class CalendarRepositoryImpl: CalendarRepository {
 
     // ここで最低限の処理しか返さないようにすれば問題なくね??
     override fun getCurrentMonth(currentMonth: Int): Flow<List<Pair<Int, String>>> {
-        val days = mutableListOf<String>()
+
         val calendar = Calendar.getInstance(Locale.JAPAN)
         calendar.set(Calendar.MONTH, currentMonth)
         calendar.set(Calendar.DAY_OF_MONTH, currentMonth)
-        calendar.set(Calendar.DAY_OF_WEEK, currentMonth)
+
         val daysOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
         val simpleDateFormat = SimpleDateFormat("MM-dd")
         val currentMonthList = mutableListOf<Pair<Int, String>>()
         for (i in 0 until daysOfMonth) {
             calendar.set(Calendar.DAY_OF_MONTH, i + 1)
             Log.d("Calendar1", "${simpleDateFormat.format(calendar.time)}")
-            days.add("${simpleDateFormat.format(calendar.time)}")
             currentMonthList.add(Pair(calendar.get(Calendar.DAY_OF_WEEK), simpleDateFormat.format(calendar.time)))
         }
         // 曜日で抜き出す数を変えればよい
         // 抜き出す数を左から日-土で示す
         // 1, 2, 3, 4, 5, 6, 0
+
+        Log.d("CalendarRepo1", "$currentMonthList")
         return flow {
             emit(currentMonthList.toList())
         }
@@ -46,9 +47,11 @@ class CalendarRepositoryImpl: CalendarRepository {
         val prevMonth = currentMonth - 1
         prevCalendar.set(Calendar.MONTH, prevMonth)
         prevCalendar.set(Calendar.DAY_OF_MONTH, prevMonth)
+        val daysOfMonth = prevCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)
 
         val lastDayCalendarFlag = Calendar.getInstance(Locale.JAPAN)
         lastDayCalendarFlag.set(Calendar.MONTH, prevMonth)
+        lastDayCalendarFlag.set(Calendar.DAY_OF_MONTH, daysOfMonth)
 
         val prevCount = when (lastDayCalendarFlag.get(Calendar.DAY_OF_WEEK)) {
             1 -> {
@@ -82,18 +85,19 @@ class CalendarRepositoryImpl: CalendarRepository {
         val currentCalendar = Calendar.getInstance(Locale.JAPAN)
         currentCalendar.set(Calendar.MONTH, currentMonth)
         currentCalendar.set(Calendar.DAY_OF_MONTH, currentMonth)
-        currentCalendar.set(Calendar.DAY_OF_WEEK, currentMonth)
         val currentDaysOfMonth = currentCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)
 
         // next
-        // currentDaysOfMonth - 1にする必要ある??
         val last = 42 - (prevCount + currentDaysOfMonth)
-        val days = mutableListOf<String>()
+
+        Log.d("Calendar2-1", "$last")
+        Log.d("Calendar2-2", "$currentDaysOfMonth")
+        Log.d("Calendar2-3", "$prevCount")
+
         val calendar = Calendar.getInstance(Locale.JAPAN)
         val nextMonth = currentMonth + 1
         calendar.set(Calendar.MONTH, nextMonth)
         calendar.set(Calendar.DAY_OF_MONTH, nextMonth)
-        val daysOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
         val simpleDateFormat = SimpleDateFormat("MM-dd")
 
         // ループを止める範囲を変更する
@@ -103,16 +107,17 @@ class CalendarRepositoryImpl: CalendarRepository {
         for (i in 0 until last) {
             calendar.set(Calendar.DAY_OF_MONTH, i + 1)
             Log.d("Calendar2", "${simpleDateFormat.format(calendar.time)}")
-            days.add("${simpleDateFormat.format(calendar.time)}")
             currentMonthList.add(Pair(calendar.get(Calendar.DAY_OF_WEEK), simpleDateFormat.format(calendar.time)))
         }
+
+        Log.d("CalendarRepo2", "$currentMonthList")
+
         return flow {
             emit(currentMonthList.toList())
         }
     }
 
     override fun getPreviousMonth(currentMonth: Int): Flow<List<Pair<Int, String>>> {
-        val days = mutableListOf<String>()
         val calendar = Calendar.getInstance(Locale.JAPAN)
         val prevMonth = currentMonth - 1
         calendar.set(Calendar.MONTH, prevMonth)
@@ -123,6 +128,8 @@ class CalendarRepositoryImpl: CalendarRepository {
         val lastDayCalendarFlag = Calendar.getInstance(Locale.JAPAN)
         lastDayCalendarFlag.set(Calendar.MONTH, prevMonth)
         lastDayCalendarFlag.set(Calendar.DAY_OF_MONTH, daysOfMonth)
+
+        Log.d("Calendar3434", "${lastDayCalendarFlag.get(Calendar.DAY_OF_WEEK)}")
 
         val start = when (lastDayCalendarFlag.get(Calendar.DAY_OF_WEEK)) {
             1 -> {
@@ -158,10 +165,12 @@ class CalendarRepositoryImpl: CalendarRepository {
             for (i in start until daysOfMonth) {
                 calendar.set(Calendar.DAY_OF_MONTH, i + 1)
                 Log.d("Calendar3", "${simpleDateFormat.format(calendar.time)}")
-                days.add("${simpleDateFormat.format(calendar.time)}")
                 currentMonthList.add(Pair(calendar.get(Calendar.DAY_OF_WEEK), simpleDateFormat.format(calendar.time)))
             }
         }
+
+        Log.d("CalendarRepo3", "$currentMonthList")
+
         return flow {
             emit(currentMonthList.toList())
         }
