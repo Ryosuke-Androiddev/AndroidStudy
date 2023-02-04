1aff4d20cb7ee42377c84aed9764ea58940aebe5
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-02-04 20:54:12 +0900

    [TodoApp] Implement Previous, Current, Next Month Calendar Domain Logic

--- app/src/main/java/com/example/androidestudy/feature/todoapp/domain/usecase/todo/GetCalendarDays.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/domain/usecase/todo/GetCalendarDays.kt
@@ -1,10 +1,16 @@
 package com.example.androidestudy.feature.todoapp.domain.usecase.todo
 
+import android.util.Log
 import com.example.androidestudy.feature.todoapp.domain.repository.CalendarRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.forEach
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import java.util.Calendar
+import java.util.Locale
 import javax.inject.Inject
 
 class GetCalendarDays @Inject constructor(
@@ -12,7 +18,11 @@ class GetCalendarDays @Inject constructor(
 ) {
     // 曜日で先月の取得したい日を取得する
     // 同じように月末のにひちも取得したいところを取得する
-    operator fun invoke(currentMonth: Int): Flow<List<String>> = flow {
+    suspend operator fun invoke(): Flow<List<String>> {
+
+        val calendarInstance = Calendar.getInstance(Locale.JAPAN)
+        val currentMonth = calendarInstance.get(Calendar.MONTH)
+        Log.d("Calendar2-2-Month", "$currentMonth")
 
         val prevCalendar = mutableListOf<String>()
         val currentCalendar = mutableListOf<String>()
@@ -21,14 +31,16 @@ class GetCalendarDays @Inject constructor(
         val calendar = mutableListOf<String>()
 
         // 3つ同時に参照した場合、特に処理は必要ない
-        repository.getPreviousMonth(currentMonth = currentMonth).onEach { prevMonthList ->
+        val prevCalendarFlow = repository.getPreviousMonth(currentMonth = currentMonth).onEach { prevMonthList ->
+            Log.d("CalendarUseCaseF1", "$prevMonthList")
             prevMonthList.forEach { datePair ->
                 // 最後のキリ良く終わっていない部分を追加する
                 prevCalendar.add(datePair.second)
             }
         }
 
-        repository.getCurrentMonth(currentMonth = currentMonth).onEach { currentMonthList ->
+        val currentCalendarFlow = repository.getCurrentMonth(currentMonth = currentMonth).onEach { currentMonthList ->
+            Log.d("CalendarUseCaseF2", "$currentMonthList")
             currentMonthList.forEach { datePair ->
                 currentCalendar.add(datePair.second)
             }
@@ -36,17 +48,33 @@ class GetCalendarDays @Inject constructor(
 
         // ここは,全体で何個あるか確認して、残った分だけを追加する
         // prev, current Calendarの大きさから総グリッド数を引い多分だけをリストに追加する
-        repository.getNextMonth(currentMonth = currentMonth).onEach { prevMonthList ->
-            prevMonthList.forEach { datePair ->
+        val nextCalendarFlow = repository.getNextMonth(currentMonth = currentMonth).onEach { nextMonthList ->
+            Log.d("CalendarUseCaseF3", "$nextMonthList")
+            nextMonthList.forEach { datePair ->
                 nextCalendar.add(datePair.second)
             }
         }
 
-        // 表示したい内容を持ったリストこここで作成する
-        calendar.addAll(prevCalendar)
-        calendar.addAll(currentCalendar)
-        calendar.addAll(nextCalendar)
+        // suspendないで呼び込む
+        combine(prevCalendarFlow, currentCalendarFlow, nextCalendarFlow) { f1, f2, f3 ->
+            f1 + f2 + f3
+        }.collect {
+            it.forEach {
+                Log.d("CalendarStateCombine", "${it.second}")
+                calendar.add(it.second)
+            }
+        }
 
-        emit(calendar.toList())
+        // 表示したい内容を持ったリストこここで作成する
+
+        Log.d("CalendarStateUseCaseGet", "$calendar")
+        Log.d("CalendarStateUseCase", "$currentCalendar")
+        Log.d("CalendarStateUseCase", "$prevCalendar")
+        Log.d("CalendarStateUseCase", "$nextCalendar")
+
+        // ここがちゃんと追加されていない
+        return flow {
+            emit(calendar.toList())
+        }
     }
 }
--- app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/viewmodel/DailyViewModel.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/viewmodel/DailyViewModel.kt
@@ -1,18 +1,40 @@
 package com.example.androidestudy.feature.todoapp.presentation.todo.daily.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.androidestudy.feature.todoapp.domain.usecase.todo.GetCalendarDays
 import com.example.androidestudy.feature.todoapp.presentation.todo.list.component.DailyState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class DailyViewModel(
+class DailyViewModel @Inject constructor(
     private val getCalendarDays: GetCalendarDays
 ): ViewModel() {
 
+    init {
+        getCurrentCalendar()
+    }
+
     var state by mutableStateOf(DailyState())
         private set
+
+    private fun getCurrentCalendar() {
+        viewModelScope.launch {
+            getCalendarDays().onEach { calendar ->
+                Log.d("CalendarViewModel", "$calendar")
+                state = state.copy(
+                    calendar = calendar
+                )
+            }.launchIn(viewModelScope)
+        }
+    }
 }
