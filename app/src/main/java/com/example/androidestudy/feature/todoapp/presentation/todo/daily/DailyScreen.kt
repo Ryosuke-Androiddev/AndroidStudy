c07340f400bf77b1deeae90ae3e230538f729b75
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-02-06 23:53:17 +0900

    [TodoApp] Show Calendar result

--- app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/DailyScreen.kt
+++ app/src/main/java/com/example/androidestudy/feature/todoapp/presentation/todo/daily/DailyScreen.kt
@@ -1,79 +1,42 @@
 package com.example.androidestudy.feature.todoapp.presentation.todo.daily
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.androidestudy.feature.todoapp.presentation.todo.daily.component.MonthGridLine
 import com.example.androidestudy.feature.todoapp.presentation.todo.daily.component.ScheduleHeader
+import com.example.androidestudy.feature.todoapp.presentation.todo.daily.viewmodel.DailyViewModel
 
 @Composable
 fun DailyScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: DailyViewModel = hiltViewModel()
 ) {
+
+    val state = viewModel.state
+    Log.d("CalendarStateDaily", "${state.calendar}")
+    Log.d("CalendarStateDaily", "${state.isLoading}")
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         ScheduleHeader(navController = navController, month = "2月")
         Spacer(modifier = Modifier.height(4.dp))
 
-        val calendar = listOf(
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18,
-            19,
-            20,
-            21,
-            22,
-            23,
-            24,
-            25,
-            26,
-            27,
-            28,
-            29,
-            30,
-            31
-        )
-
-        val nextCalendar = listOf(
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-        )
-
-        // 表示の変換を行うドメイン知識が必要
-        MonthGridLine(
-            calendar,
-            nextCalendar
-        )
+        // ここの動機処理を考える
+        if (state.calendar.size > 30) {
+            MonthGridLine()
+        }
     }
 }
